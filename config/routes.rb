4487bd47d451248f1815cb66aff748784780cdaf
Mark <mark.f.bennett@gmail.com>
2010-05-08 12:08:08 -0600

    user creation

--- config/routes.rb
+++ config/routes.rb
@@ -32,8 +32,11 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-
+  map.resource :account, :controller => "users"
+  map.resources :users
   # See how all your routes lay out with "rake routes"
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
